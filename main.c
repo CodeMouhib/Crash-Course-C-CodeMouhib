29c6f28bd54120a9007eb174f8466c4cdd8b35b5
MOUHIB <mouhib_otm.upf.ac.ma>
2023-11-03 14:48:48 +0100

    PushUp 1

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
 void main() {
-    printf("Salam, A l3alam !\n");
-    
+
+printf("Salam, A l3alam !\n");
+
+int a ;
+
+a = 20;
+
+printf("valeur diyal variable a = %d \n",a);
+
 }
