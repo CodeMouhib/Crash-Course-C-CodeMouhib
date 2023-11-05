3793c52f5101b49b2d201ab17626eb4e54f20bed
MOUHIB <mouhib_otm.upf.ac.ma>
2023-11-05 14:26:57 +0100

    PushUp 5

--- main.c
+++ main.c
@@ -5,28 +5,27 @@ void main() {
 printf("Salam, A l3alam !\n");
 
 
-int a ;
+float a ;
 
-printf("\n hadi a7ssan claculatrice fl3alam wakha ghat7sseb gher lmjmo3");
+printf("\n hadi a7ssan claculatrice fl3alam wakha ghat7sseb gher l9isma");
 
 printf("\n Asidi 3tini ra9m lowel : ");
+scanf("%f", &a);
 
-scanf("%d", &a);
+printf("\n valeur diyal Ra9m Lowel = %f \n",a);
 
-printf("\n valeur diyal Ra9m Lowel = %d \n",a);
-
-int b;
+float b;
 
 printf("\n Asidi zidni Ra9m tani : ");
 
-scanf("%d", &b);
+scanf("%f", &b);
 
-printf("\n valeur diyal ra9m tani  = %d \n",b);
+printf("\n valeur diyal ra9m tani  = %f \n",b);
 
-int sum;
+float division;
 
-sum = a + b ;
+division = a / b ;
 
-printf("\n ra9m lowel = %d , ra9m tani = %d , lmjmo3 diyal ra9m lowel + ra9m tani = %d \n",a,b,sum);
+printf("\n ra9m lowel = %f , ra9m tani = %f , l9isma diyal ra9m lowel + ra9m tani = %f \n",a,b,division);
 
 }
