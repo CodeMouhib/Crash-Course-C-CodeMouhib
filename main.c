ca1cc7b7a1ae4be104be7de7bf132959d8373379
MOUHIB <mouhib_otm.upf.ac.ma>
2023-11-05 14:34:43 +0100

    PushUp 6

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ printf("\n hadi a7ssan claculatrice fl3alam wakha ghat7sseb gher l9isma");
 printf("\n Asidi 3tini ra9m lowel : ");
 scanf("%f", &a);
 
-printf("\n valeur diyal Ra9m Lowel = %f \n",a);
+printf("\n valeur diyal Ra9m Lowel = %.2f \n",a);
 
 float b;
 
@@ -20,12 +20,12 @@ printf("\n Asidi zidni Ra9m tani : ");
 
 scanf("%f", &b);
 
-printf("\n valeur diyal ra9m tani  = %f \n",b);
+printf("\n valeur diyal ra9m tani  = %.2f \n",b);
 
 float division;
 
 division = a / b ;
 
-printf("\n ra9m lowel = %f , ra9m tani = %f , l9isma diyal ra9m lowel + ra9m tani = %f \n",a,b,division);
+printf("\n ra9m lowel = %.2f , ra9m tani = %.2f , l9isma diyal ra9m lowel + ra9m tani = %.2f \n",a,b,division);
 
 }
