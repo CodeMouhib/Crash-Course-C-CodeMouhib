c1378aa6c861ffc415c7d370195c8c7bee49edcc
MOUHIB <mouhib_otm.upf.ac.ma>
2023-11-06 12:40:04 +0100

    PushUp 1

--- main.c
+++ main.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
 void main() {
-    
+
+    char ch1, ch2;
+
+    // Prompt the user for input
+    printf("asidi 3tini chi charcter  : ");
+    scanf(" %c", &ch1); // Note the space before %c to consume any leading whitespace
+
+    // Display the input character
+    printf("asidi yeak hada character li 3titini : %c\n", ch1);
+
+    // Increment the character and display the result
+    ch2 = ch1 + 1;
+    printf("7et program diyali 9ari ha character li kayji morah : %c\n", ch2);
+
+
 }
