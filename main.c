d5b12e3b668c5430d348f87cbb8cfef0051d9810
MOUHIB <mouhib_otm.upf.ac.ma>
2023-11-06 15:27:34 +0100

    PushUp 1

--- main.c
+++ main.c
@@ -2,5 +2,24 @@
 
 void main()
 {
-	
+    printf("Lyoum Ghanbiyenlk program diyalk Ch7al wella dki\n");
+    printf("ghan7sblk lmjmo3 diyal l2ar9am mn 1 tal fin ma bghiti \n");
+    
+    int n, i;
+    int sum = 0;
+
+    printf("tal fin bghitini n7ssblk : ");
+    scanf("%d", &n);
+    
+    printf("\nsbar 3lia n7ssb lmjmo3 diyal ar9am mn 0 tal %d \n",n);
+
+    // Using a for loop to calculate the sum of numbers from 1 to n
+    for (i = 1; i <= n; i++) {
+        sum += i; //sum = sum + i
+    }
+
+    printf("\nlmjmo3 mn  0 tal %d howa : %d\n", n, sum);
+
+    printf("haaa kijitek, hsseb m3aya t2eked belli maghletch !\n");
+
 }
